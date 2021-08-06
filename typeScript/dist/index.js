3b578418a5814cb0b2bc92d21c99f49b21e11a33
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-06 17:52:00 -0300

    inicio da criacao dos blocos de sustentacao

---
+++ typeScript/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var http_1 = require("http");
+// Definicao de porta
+var port = 5000;
+var server = http_1.createServer(function (request, response) {
+    // Implementar o código aqui.
+});
+// Execução
---
+++ typeScript/index.ts
@@ -0,0 +1,15 @@
+// Importacao de bibliotecas
+import * as queryString from 'query-string';
+import * as url from 'url';
+import * as fs from 'fs';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
+// Definicao de porta
+const port = 5000;
+
+const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+
+    // Implementar o código aqui.
+});
+
+// Execução
