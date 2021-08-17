c45a0a95f900cbcfa16eefa5ce6420f1096eb596
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-17 09:54:13 -0300

    criacao e atualizacao usuario

--- typeScript/index.ts
+++ typeScript/index.ts
@@ -1,15 +1,44 @@
 // Importacao de bibliotecas
-import * as queryString from 'query-string';
+import { parse } from 'query-string';
 import * as url from 'url';
-import * as fs from 'fs';
+import { writeFile } from 'fs';
 import { createServer, IncomingMessage, ServerResponse } from 'http';
 
 // Definicao de porta
 const port = 5000;
 
 const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+// decodificação da url com ternário:
+    const urlparse = url.parse(request.url ? request.url : '', true);
 
+    var resposta;
+
+    // Receber informacoes do usuario
+    const params = parse(urlparse.search ? urlparse.search : '');
+
+    // Criar um usuario - Atualizar o usuario
+    if(urlparse.pathname == '/criar-atualizar-usuario'){
+
+        // salvar/atualizar informacoes
+        writeFile('users/' + params.id + '.txt', JSON.stringify(params), function (err: any) {
+            if (err) throw err;
+            console.log('Saved!');
+      
+            resposta = 'Usuario criado/atualizado com sucesso';
+            
+            response.statusCode = 200; 
+            response.setHeader('Content-Type', 'text/plain');
+            response.end(resposta);
+          });
+    }      
+
+    // response.end("Hello World");
     // Implementar o código aqui.
 });
 
 // Execução
+server.listen( port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+// http://localhost:5000/criar-atualizar-usuario?id=32&nome=amauri
